23c32520f10595808227f6d81c9b0bc16dd6663a
Julian <me@julian.pro>
2020-05-14 22:54:29 +0200

    Id to ID

--- currency_translation.go
+++ currency_translation.go
@@ -4,7 +4,7 @@ package shopware
 // https://github.com/shopware/platform/blob/6.2/src/Core/System/Currency/Aggregate/CurrencyTranslation/CurrencyTranslationEntity.php
 type CurrencyTranslation struct {
 	TranslationEntity
-	CurrencyId string    `json:"currencyId"`
+	CurrencyID string    `json:"currencyId"`
 	ShortName  string    `json:"shortName"`
 	Name       string    `json:"name"`
 	Currency   *Currency `json:"currency"`
